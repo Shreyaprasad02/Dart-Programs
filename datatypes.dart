ade008d7cbd7d75fc98562b6db8d26c943ea8f83
Shreya <shreyaprasadaug@gmail.com>
2023-04-12 17:33:10 +0530

    Initial commit

--- datatypes.dart
+++ datatypes.dart
@@ -1,8 +1,9 @@
 void main(){
-  int n = 10;
+  /*int n = 10;
   double d = 5.5;
   String s2 = "               some\n thing ";
-  //multiline string
+
+ //multiline string
  String s1 = '''I am shreya
 
  Who are you?''';
@@ -12,5 +13,12 @@ void main(){
  print(s2.trim());
  print(s2.isEmpty);
  print(s2.toUpperCase());
- print(s2.toUpperCase());
+ print(s2.toUpperCase());*/
+
+ int num = 5;
+ double d = 5.6;
+ print(d.ceil());
+ print(d.floor());
+ print(d.truncate());
+
 }
