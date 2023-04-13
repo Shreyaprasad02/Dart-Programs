d04eb9809327bddbd0b2631c78f1520f74577a8b
Shreya <shreyaprasadaug@gmail.com>
2023-04-13 17:22:58 +0530

    Initial commit

---
+++ list_and_map.dart
@@ -0,0 +1,10 @@
+void main(){
+  //static list
+  List<int> first = [1, 2, 3, 4, 5]; 
+  //dynamic list
+  List second = ['aa', 'bb', 'cc', 'dd'];
+  second.add('ee');
+  second.add(24);
+  print(second);
+  
+}
