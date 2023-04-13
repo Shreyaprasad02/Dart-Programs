675cf8e596ea4f6060622a2eb25a583f40aecdc5
Shreya <shreyaprasadaug@gmail.com>
2023-04-13 17:35:20 +0530

    Initial commit

--- list_and_map.dart
+++ list_and_map.dart
@@ -3,8 +3,16 @@ void main(){
   List<int> first = [1, 2, 3, 4, 5]; 
   //dynamic list
   List second = ['aa', 'bb', 'cc', 'dd'];
+  //list methods
   second.add('ee');
   second.add(24);
-  print(second);
+  second.remove('dd');
+  print(second.isEmpty);
+  print(second[3]);
   
+  // printing elements of list second
+  for(int i = 0; i<second.length; i++){
+    print(second[i]);
+  };
+
 }
