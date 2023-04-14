4b0fbe3b33588dc01fc01285d8a5134af6dae7a3
Shreya <shreyaprasadaug@gmail.com>
2023-04-14 12:52:43 +0530

    Initial commit

--- map.dart
+++ map.dart
@@ -6,5 +6,13 @@ void main(){
   };
 
   first['age'] = 12;
+  first['id'] = 42;
   print(first);
+
+  // methods in map
+  print(first.length);
+  print(first.isEmpty);
+
+  first.forEach((k,v) => print('$k : $v'));
+  
 }
