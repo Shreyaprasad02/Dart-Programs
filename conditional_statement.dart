a98e60fb6331996e1659356a852661913d419d53
Shreya <shreyaprasadaug@gmail.com>
2023-04-16 22:38:29 +0530

    Initial commit

--- conditional_statement.dart
+++ conditional_statement.dart
@@ -9,7 +9,13 @@ void main(){
     case 3: print('3');
     break;
     default: print('Invalid Number');
-    
   }
-
-}
+  // if statement
+  int a = 10;
+  if(a >= 5){
+    print('t');
+  }
+  else{
+    print('not satisfied');
+    }
+  }
