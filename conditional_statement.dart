0431b9b1bab6a6f77c90977a6cd68124527c3bd3
Shreya <shreyaprasadaug@gmail.com>
2023-04-16 22:29:35 +0530

    Initial commit

---
+++ conditional_statement.dart
@@ -0,0 +1,15 @@
+// conditional statements in dart
+void main(){
+  //swutch statement
+  switch(2){
+    case 5: print('5');
+    break;
+    case 4: print('4');
+    break;
+    case 3: print('3');
+    break;
+    default: print('Invalid Number');
+    
+  }
+
+}
