27ac0b5eb37a526e3fe8fd9ff99b3208f9bf0b41
Shreya <shreyaprasadaug@gmail.com>
2023-04-18 18:11:07 +0530

    Initial commit

--- constructor.dart
+++ constructor.dart
@@ -1,15 +1,18 @@
 void main(){
-  Class some = Class();
+  Class some = Class("Shreya", 47);
   some.student();  
 }
 
 class Class{
-  //String _name = "Shreya";            //adding underscore to the variable makes it private
-  String name = "Shreya";
-  int rollNo = 47;
+  //adding underscore to the variable makes it private
+  //String _name = "Shreya";            
+  //creating a constructor
+  Class(this.name, this.rollNo);
+  String name;
+  int rollNo;
 
   student(){
-    print("Name of student is ${name}");
+    print("Name of student is ${name} and Roll No. is ${rollNo}");
   } 
 }
 
