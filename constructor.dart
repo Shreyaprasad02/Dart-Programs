3f601d0a6754749ae5484794a0118e7cf93230a0
Shreya <shreyaprasadaug@gmail.com>
2023-04-18 17:56:42 +0530

    Initial commit

--- constructor.dart
+++ constructor.dart
@@ -1,3 +1,15 @@
 void main(){
-  
+  Class some = Class();
+  some.student();  
 }
+
+class Class{
+  //String _name = "Shreya";            //adding underscore to the variable makes it private
+  String name = "Shreya";
+  int rollNo = 47;
+
+  student(){
+    print("Name of student is ${name}");
+  } 
+}
+
