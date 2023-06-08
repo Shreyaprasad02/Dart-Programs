2c88cc3ceed498601146bc91c2e0cab0d8a58e51
Shreya Prasad <87193512+Shreyaprasad02@users.noreply.github.com>
2023-06-08 13:43:00 +0530

    Update variable.dart

--- variable.dart
+++ variable.dart
@@ -1,9 +1,27 @@
 void main(){
-  //Static variable
+ //Static variable
   var a = 5;
   var b = 6;
-  //Dynamic Variable
+ //Dynamic Variable
   var name;
   name = "Shreya";
   print(a+b);
+  
+  int num1 = 2;
+  double num2 = 0.2;
+  bool value = true;
+  String name = 'Shreya';
+  
+ //Reassignable variable
+  var username;
+  
+  final String fullname = 'Ray';
+  fullname = 'ray'; //error
+  
+  const int age = 24;
+  
+  print((num1 + num2) is int);
+  print((num1 + num2).runtimeType);
+  print('My name is $name ${name is String}');
 }
+
